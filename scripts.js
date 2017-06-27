2e098455323ac8fdefd9ee6dda71871f357ac782
Malgorzata Groves <bromba1@tlen.pl>
2017-06-27 09:55:34 +0200

    new file for 11.4

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function Phone(brand, price, color, availability) {
+    //Here we put interior of the constructing function.
+    this.brand = brand; 
+	//by using this, the "brand" property of the object we create will assume the value of the brand argument
+	this.price = price;
+	this.color = color;
+
+	// probowalam miec wydruk in the shop lub out of stock, ale nie wiem jak poprawnie zakodowac ten warunek!
+	this.availability = availability; if (availability === 1) {"in the shop"} else {"out of stock"}
+}
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " and avaiability is " + this.availability + ".");
+}
+var SamsungGalaxyS6 = new Phone ("Samsung", 1600, "black", 1);
+var iPhone6S = new Phone ("Apple", 2300, "pink", 1);
+var OnePlusOne = new Phone ("onePlus", 3000, "white", 0);
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
